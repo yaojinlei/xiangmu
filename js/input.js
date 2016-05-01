7bbfd4af3de553ae2a4fd7829b73b38ce68b294e
yaojinlei <281097237@qq.com>
2016-05-01 12:34:40 +0800

    项目

---
+++ js/input.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/4/27.
+ */
+var main=document.getElementById('main-search');
+var input=main.getElementsByTagName('search-input');
+
+input.onclick=function(){
+    this.type=""
+};
+
+
